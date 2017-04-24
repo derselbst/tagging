93052780fca88cc815fa42c1fa5e4b5151be83c5
Tom M <tom.mbrt@googlemail.com>
2017-04-24 20:51:56 +0200

    add cbfd

---
+++ cbfd.sh
@@ -0,0 +1,123 @@
+# conkers bad fur day (N64)
+
+PSFPOINT=/home/tom/Downloads/PSFPoint103/psfpoint
+
+$PSFPOINT -length=10:20 sparse3d.miniusf
+$PSFPOINT -title="Rock Solid" sparse3d.miniusf
+
+$PSFPOINT -length=2:53 sparse01d.miniusf
+$PSFPOINT -title=Windyd sparse01d.miniusf
+
+$PSFPOINT -length=2:53 sparse01c.miniusf
+$PSFPOINT -title=Windyc sparse01c.miniusf
+
+$PSFPOINT -length=2:53 sparse01b.miniusf
+$PSFPOINT -title=Windyb sparse01b.miniusf
+
+$PSFPOINT -length=2:53 sparse01a.miniusf
+$PSFPOINT -title=Windya sparse01a.miniusf
+
+$PSFPOINT -length=2:53 sparse01e.miniusf
+$PSFPOINT -title=Windye sparse01e.miniusf
+
+$PSFPOINT -length=2:53 sparse01.miniusf
+$PSFPOINT -title=Windy sparse01.miniusf
+
+$PSFPOINT -length=2:15 sparse02.miniusf
+$PSFPOINT -title="Mrs. Bee" sparse02.miniusf
+
+$PSFPOINT -length=1:13 sparse0a.miniusf
+$PSFPOINT -title=Peeing sparse0a.miniusf
+
+$PSFPOINT -length=1:10 sparse15.miniusf
+$PSFPOINT -title=GameBoy1 sparse15.miniusf
+
+$PSFPOINT -length=1:07 sparse16.miniusf
+$PSFPOINT -title=GameBoy2 sparse16.miniusf
+
+$PSFPOINT -length=1:13 sparse17.miniusf
+$PSFPOINT -title=GameBoy3 sparse17.miniusf
+
+$PSFPOINT -length=3:27 sparse35.miniusf
+$PSFPOINT -title=Sloprano sparse35.miniusf
+
+$PSFPOINT -length=1:35 sparse46.miniusf
+$PSFPOINT -title="Sloprano at Night" sparse46.miniusf
+
+$PSFPOINT -length=0:29 sparse36.miniusf
+$PSFPOINT -fade=0 sparse36.miniusf
+$PSFPOINT -title=Logo sparse36.miniusf
+
+$PSFPOINT -length=1:17 sparse37.miniusf
+$PSFPOINT -title=Drunken sparse37.miniusf
+
+$PSFPOINT -length=4:30 sparse42.miniusf
+$PSFPOINT -title="Chucky Poo's Lament" sparse42.miniusf
+
+$PSFPOINT -length=4:30 sparse42a.miniusf
+$PSFPOINT -title="Chucky Poo's Lament Instrumental" sparse42a.miniusf
+
+$PSFPOINT -length=3:23 sparse5a.miniusf
+$PSFPOINT -title="Count Batula" sparse5a.miniusf
+
+$PSFPOINT -length=1:05 sparse61.miniusf
+$PSFPOINT -title="Ambient - Poo" sparse61.miniusf
+
+$PSFPOINT -length=3:21 sparse62.miniusf
+$PSFPOINT -title="Bugger Lugs" sparse62.miniusf
+
+$PSFPOINT -length=0:32 sparse81.miniusf
+
+$PSFPOINT -length=2:13 sparse18.miniusf
+$PSFPOINT -fade=0 sparse18.miniusf
+$PSFPOINT -title="Introduction - Conker The King" sparse18.miniusf
+
+$PSFPOINT -length=0:37 sparse05.miniusf
+$PSFPOINT -fade=0 sparse05.miniusf
+
+$PSFPOINT -length=0:21 sparse07.miniusf
+$PSFPOINT -fade=0 sparse07.miniusf
+
+$PSFPOINT -length=0:05 sparse0e.miniusf
+$PSFPOINT -fade=0 sparse0e.miniusf
+
+$PSFPOINT -length=1:00 sparse12.miniusf
+$PSFPOINT -fade=0 sparse12.miniusf
+
+$PSFPOINT -length=1:59 sparse1f.miniusf
+$PSFPOINT -title="Spooky" sparse1f.miniusf
+
+$PSFPOINT -length=0:03 sparse26.miniusf
+$PSFPOINT -fade=0 sparse26.miniusf
+
+$PSFPOINT -length=0:04 sparse2a.miniusf
+$PSFPOINT -fade=0 sparse2a.miniusf
+
+$PSFPOINT -length=0:04 sparse2b.miniusf
+$PSFPOINT -fade=0 sparse2b.miniusf
+
+$PSFPOINT -length=0:03 sparse27.miniusf
+$PSFPOINT -fade=0 sparse27.miniusf
+
+$PSFPOINT -length=0:06 sparse28.miniusf
+$PSFPOINT -fade=0 sparse28.miniusf
+
+$PSFPOINT -length=0:05 sparse29.miniusf
+$PSFPOINT -fade=0 sparse29.miniusf
+
+$PSFPOINT -length=0:02 sparse51.miniusf
+$PSFPOINT -fade=0 sparse51.miniusf
+$PSFPOINT -title="SFX - Mono" sparse51.miniusf
+
+$PSFPOINT -length=0:02 sparse52.miniusf
+$PSFPOINT -fade=0 sparse52.miniusf
+$PSFPOINT -title="SFX - Stereo" sparse51.miniusf
+
+$PSFPOINT -length=0:04 sparse52.miniusf
+$PSFPOINT -fade=0 sparse52.miniusf
+$PSFPOINT -title="SFX - Surround" sparse51.miniusf
+
+$PSFPOINT -length=3:12 sparse5b.miniusf
+$PSFPOINT -fade=0 sparse5b.miniusf
+$PSFPOINT -title="Zombie Cemetery" sparse5b.miniusf
+
