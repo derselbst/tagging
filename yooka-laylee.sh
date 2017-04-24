c7395265a1474683c4e32dc8dc907f38e53d3ecc
derselbst <tom.mbrt@googlemail.com>
2017-04-24 20:32:43 +0200

    initial commit

---
+++ yooka-laylee.sh
@@ -0,0 +1,358 @@
+ 
+mv 13954808.wem "Tribalstack Tropics - Nimble Race 1. wem"
+mv 14799336.wem "Tribalstack Tropics - Monument??. wem"
+mv 22160869.wem "Retrox - ? (reverse).wem"
+mv 31189334.wem "Glitterglaze Glacier - ??.wem"
+mv 47632872.wem "Retrox - ?1 (reverse).wem"
+mv 49294568.wem "Ambiance - Water.wem"
+mv 53628394.wem "Ambiance - Thunder.wem"
+mv 53635858.wem "Tribalstack Tropics - ??? Temple?.wem"
+mv 60249255.wem "Retrox - ?2 (reverse).wem"
+mv 63269928.wem "Tribalstack Tropics??? - Aquatic.wem"
+mv 72868789.wem "Retrox - ?3 (reverse).wem"
+
+mv 101536534.wem "??? - Aquatic.wem"
+mv 112592897.wem "David Wise's leaked thing.wem"
+mv 113086865.wem "Collect Playtonic Coin.wem"
+mv 121161360.wem "some reverse.wem"
+mv 134263814.wem "some aquatic.wem"
+
+mv 143756378.wem "Retrox - ?4 (reverse).wem"
+
+mv 162230282.wem "Tribalstack Tropics - Intro.wem"
+mv 162895803.wem "Tribalstack Tropics - Aquatic.wem"
+
+mv 191161704.wem "David Wise's leaked thing 2.wem"
+mv 199435315.wem "Hivory Towers - ??.wem"
+
+mv 217686622.wem "Tribalstack Tropics - ???.wem"
+mv 225243248.wem "Tribalstack Tropics - ???.wem"
+
+
+mv 238147918.wem "Tribalstack Tropics - Nimble Race 1.wem"
+mv 240416347.wem "Tribalstack Tropics - Aquatic Intro.wem"
+
+mv 250663170.wem "Moodymaze Marsh - Aquatic.wem"
+
+mv 69984211.wem "Moodymaze Marsh - Some Cave???.wem"
+mv 84745841.wem "Moodymaze Marsh - Main.wem"
+mv 251664514.wem "Moodymaze Marsh - ??.wem"
+mv 253636843.wem "Moodymaze Marsh - ???.wem"
+
+mv 257626415.wem "Tribalstack Tropics - ????.wem"
+
+mv 271662028.wem "Tribalstack Tropics - Main.wem"
+mv 278600966.wem "Slurp State ended.wem"
+mv 287648828.wem "TribalStack Tropics - Monument?.wem"
+
+mv 297309298.wem "Glitterglaze Glacier - Main.wem"
+mv 297472422.wem "Hivory Towers - Main.wem"
+mv 298777168.wem "Tribalstack Tropics - Boss - The Great Rampo.wem"
+
+mv 304269507.wem "Retrox - ?4 (reverse?).wem"
+mv 332178222.wem "Retrox - ?5 (reverse).wem"
+
+
+mv 332971003.wem "Main Theme.wem"
+
+mv 353467983.wem "Death.wem"
+mv 357001001.wem "Glitterglaze Glacier - Main Intro.wem"
+mv 362717196.wem "Retrox - ?6 (reverse).wem"
+
+mv 386756905.wem "Tribalstack Tropics - Boss - The Great Rampo again.wem"
+mv 393135791.wem "Shipwreck Creek.wem"
+mv 395580153.wem "Tribalstack Tropics - Nimble Race 1.wem"
+mv 398883026.wem "Tribalstack Tropics - Monument Intro.wem"
+
+mv 405609706.wem "Retrox Arcade.wem"
+mv 405732086.wem "Glitterglaze Glacier - Boss - Brrreeze Blok.wem"
+mv 406031902.wem "Collect Pagie.wem"
+
+1003159852.wem
+1003471996.wem
+1004175105.wem
+1004707711.wem
+1005096691.wem
+1005700745.wem
+1006406196.wem
+1008935718.wem
+1010807917.wem
+1012144986.wem
+1014835385.wem
+1015079427.wem
+
+1018588088.wem
+1022251749.wem
+1025733604.wem
+1029255641.wem
+1035426249.wem
+1038850781.wem
+1039566825.wem
+1041573570.wem
+1048738159.wem
+1051348951.wem
+1052765802.wem
+105394285.wem
+1055128299.wem
+1055798865.wem
+1058196123.wem
+1066961635.wem
+1069126312.wem
+107947895.wem
+109632540.wem
+110665772.wem
+116497321.wem
+118536890.wem
+119481731.wem
+119931352.wem
+120153550.wem
+123687081.wem
+134910693.wem
+137601607.wem
+137821641.wem
+142316651.wem
+142937143.wem
+148483046.wem
+178163251.wem
+186598022.wem
+189166703.wem
+189923256.wem
+206140085.wem
+206397749.wem
+216346268.wem
+220691559.wem
+225660197.wem
+231715424.wem
+232061258.wem
+234021018.wem
+234033844.wem
+235243822.wem
+236003763.wem
+243511133.wem
+245107061.wem
+263283307.wem
+285554117.wem
+285625361.wem
+286271761.wem
+288116429.wem
+290271322.wem
+291268796.wem
+292203128.wem
+302365459.wem
+313799732.wem
+319733823.wem
+328632575.wem
+333680004.wem
+334721071.wem
+337472753.wem
+340906762.wem
+344059508.wem
+351730250.wem
+369183907.wem
+369864684.wem
+370198222.wem
+376828056.wem
+377232333.wem
+382241610.wem
+389110418.wem
+400363890.wem
+406479001.wem
+406587979.wem
+409663388.wem
+410801645.wem
+412038225.wem
+416075368.wem
+419992515.wem
+424034435.wem
+425157647.wem
+431439255.wem
+431661253.wem
+446598529.wem
+457968170.wem
+461690759.wem
+468160550.wem
+471112338.wem
+473513511.wem
+478008553.wem
+478721635.wem
+479353201.wem
+480460865.wem
+484951464.wem
+485085160.wem
+493511128.wem
+503171492.wem
+503796139.wem
+504929530.wem
+505173011.wem
+509096205.wem
+511099044.wem
+512165635.wem
+512189928.wem
+517798599.wem
+518448063.wem
+521037595.wem
+523359880.wem
+524597222.wem
+529464369.wem
+532707470.wem
+533380088.wem
+533849150.wem
+538090783.wem
+540059236.wem
+540650590.wem
+547612741.wem
+549400600.wem
+551659470.wem
+552625626.wem
+554160212.wem
+556002834.wem
+565897698.wem
+570519491.wem
+581189516.wem
+583504928.wem
+589136190.wem
+592774655.wem
+594567569.wem
+596833678.wem
+604947276.wem
+605193210.wem
+606852958.wem
+608537953.wem
+609116138.wem
+612946724.wem
+615517057.wem
+619703811.wem
+626606754.wem
+629873387.wem
+630701173.wem
+631007620.wem
+635329926.wem
+636337061.wem
+641067179.wem
+643101512.wem
+647222234.wem
+664424705.wem
+665137701.wem
+676601034.wem
+676958470.wem
+677278999.wem
+680176587.wem
+680649174.wem
+681961272.wem
+683031846.wem
+685969381.wem
+686836984.wem
+688300463.wem
+691969138.wem
+693539848.wem
+695192682.wem
+697174408.wem
+701458817.wem
+706107125.wem
+710135945.wem
+715352000.wem
+717672738.wem
+717876552.wem
+720667710.wem
+721125978.wem
+722248606.wem
+722727372.wem
+725652065.wem
+726122661.wem
+726295195.wem
+728916668.wem
+733128434.wem
+733296349.wem
+735617263.wem
+736186607.wem
+737268256.wem
+737521290.wem
+739556270.wem
+740153353.wem
+740929881.wem
+745346874.wem
+751992073.wem
+756341589.wem
+757807458.wem
+762275906.wem
+763586405.wem
+765409914.wem
+766411029.wem
+766773524.wem
+770480391.wem
+771533348.wem
+772239675.wem
+773695599.wem
+779320198.wem
+782124416.wem
+782948772.wem
+783681839.wem
+783948087.wem
+784927594.wem
+791763668.wem
+793653577.wem
+794176864.wem
+795208762.wem
+803781205.wem
+816554416.wem
+833289157.wem
+836041078.wem
+836398535.wem
+838677736.wem
+839239894.wem
+844016443.wem
+847466896.wem
+849647996.wem
+855337769.wem
+861876207.wem
+865430140.wem
+868394772.wem
+875728178.wem
+878104816.wem
+880777157.wem
+883279601.wem
+889278688.wem
+890535849.wem
+904259733.wem
+905180337.wem
+905636539.wem
+907736485.wem
+910563463.wem
+910619733.wem
+911034296.wem
+911768037.wem
+913469834.wem
+915098121.wem
+921848464.wem
+922848822.wem
+925526659.wem
+926548473.wem
+927464607.wem
+927972605.wem
+930992084.wem
+938733566.wem
+939796231.wem
+940311187.wem
+942605757.wem
+943527140.wem
+943835335.wem
+949309814.wem
+950477509.wem
+951630740.wem
+953233564.wem
+956730973.wem
+956995474.wem
+961257650.wem
+968037953.wem
+969611759.wem
+972374786.wem
+972616824.wem
+983178884.wem
+987976064.wem
+988991861.wem
+993276153.wem
+995363164.wem
+996207960.wem
+997014908.wem
+997760992.wem
+997850493.wem
