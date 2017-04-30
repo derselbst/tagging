bd6bd08fde8bd424fd0e40f159db0dc79f9cb7f0
derselbst <tom.mbrt@googlemail.com>
2017-04-30 15:41:54 +0200

    update yooka-laylee.sh

--- yooka-laylee.sh
+++ yooka-laylee.sh
@@ -7,6 +7,9 @@
 # which appears to have a slightly different tune. when unlooped section played,
 # playhead rewinds to beginning of former looped section.
 
+# some files tagged with "not_used_by_game" because when doing a fuser *.wem I see
+# that these files are not opened though being in the respective world
+
 mv 22160869.wem "Rextro - ? (reverse).wem"
 mv 47632872.wem "Rextro - ?1 (reverse).wem"
 mv 49294568.wem "Ambiance - Water.wem"
@@ -21,7 +24,6 @@ mv 101536534.wem "??? - Aquatic.wem"
 mv 112592897.wem "David Wise's leaked thing.wem"
 mv 113086865.wem "Collect Playtonic Coin.wem"
 mv 121161360.wem "some reverse.wem"
-mv 134263814.wem "some aquatic.wem"
 
 mv 143756378.wem "Rextro - ?4 (reverse).wem"
 
@@ -29,6 +31,10 @@ mv 143756378.wem "Rextro - ?4 (reverse).wem"
 mv 191161704.wem "David Wise's leaked thing 2.wem"
 mv 199435315.wem "Hivory Towers - ??.wem"
 
+
+mv 589136190.wem "Kartos Challenge - ?.wem"
+mv 949309814.wem "Kartos Challenge - ??.wem"
+mv 995363164.wem "Kartos Challenge - ???.wem"
 
 
 mv 278600966.wem "SFX - Slurp State ended.wem"
@@ -40,12 +46,17 @@ mv 332178222.wem "Rextro - ?5 (reverse).wem"
 
 
 mv 332971003.wem "Main Theme.wem"
-mv 11841553.wem  "Main Theme - Ambiance - Water,Birds.wem"
+mv 910563463.wem "Main Theme - Hurry.wem"
+mv 942605757.wem "Main Theme - Hurry.wem"
+
+mv 11841553.wem  "Ambiance - Water,Birds.wem"
 
 mv 353467983.wem "SFX - Death.wem"
 mv 362717196.wem "Rextro - ?6 (reverse).wem"
 
 mv 393135791.wem "Shipwreck Creek - Normal.wem"
+mv 968037953.wem "Shipwreck Creek - Normal (unlooped).wem"
+
 mv 626606754.wem "Shipwreck Creek - Aquatic.wem"
 
 mv 405609706.wem "Rextro Arcade.wem"
@@ -59,7 +70,7 @@ mv 1052765802.wem "Ambiance - Water.wem"
 mv 370198222.wem "SFX - ???.wem"
 
 mv 446598529.wem "Ambiance.wem"
-mv 540650590.wem "Hivory Towers - Glitterglaze Glacier.wem"
+mv 540650590.wem "Hivory Towers - Interior (Glitterglaze Glacier).wem"
 mv 838677736.wem "Ambiance - Environment.wem"
 mv 890535849.wem "Hivory Towers - Aquatic.wem"
 mv 939796231.wem "Hivory Towers - Normal.wem"
@@ -77,14 +88,18 @@ mv 189166703.wem "Ambiance - Heavy Wind.wem"
 mv 1005700745.wem "Hivory Towers - ?!?.wem"
 mv 988991861.wem  "Hivory Towers - ?!?.wem" # duplicate of 10057*
 
-mv 512165635.wem "Hivory Towers - Toxic Grassland.wem"
+mv 512165635.wem "Hivory Towers - Toxic Plane.wem"
 mv 868394772.wem "Hivory Towers - To The Top.wem"
 
 
 
 mv 722248606.wem "Tribalstack Tropics - Normal (quieter,not_used_by_game).wem"
 mv 271662028.wem "Tribalstack Tropics - Normal (Early?) (long,unlooped,fadeout,not_used_by_game).wem"
+mv 431661253.wem "Tribalstack Tropics - Normal (not_used_by_game).wem"
+mv 570519491.wem "Tribalstack Tropics - Normal (not_used_by_game2).wem"
+mv 726295195.wem "Tribalstack Toopics - Normal (not_used_by_game3).wem"
 mv 533380088.wem "Tribalstack Tropics - Normal.wem"
+mv 972616824.wem "Tribalstack Tropics - Normal (Intro).wem"
 mv 636337061.wem "Tribalstack Tropics - Normal (unlooped).wem"
 
 mv 63269928.wem  "Tribalstack Tropics - Aquatic.wem"
@@ -93,39 +108,69 @@ mv 855337769.wem "Tribalstack Tropics - Duke's Temple.wem"
 mv 162895803.wem "Tribalstack Tropics - Duke's Temple (unlooped).wem"
 mv 630701173.wem "Tribalstack Tropics - Duke's Temple (again, not_used_by_game).wem"
 mv 844016443.wem "Tribalstack Tropics - Duke's Temple (again2, not_used_by_game).wem"
+mv 461690759.wem "Tribalstack Tropics - Duke's Temple (again3, not_used_by_game).wem"
+mv 473513511.wem "Tribalstack Tropics - Duke's Temple (again4, not_used_by_game).wem"
+mv 710135945.wem "Tribalstack Tropics - Duke's Temple (again5, not_used_by_game).wem"
+mv 997014908.wem "Tribalstack Tropics - Duke's Temple (again6, not_used_by_game).wem"
+
+
+
 
 mv 529464369.wem "Tribalstack Tropics - Corplet Camp.wem"
 mv 257626415.wem "Tribalstack Tropics - Corplet Camp (again, not_used_by_game).wem"
 mv 53635858.wem  "Tribalstack Tropics - Corplet Camp (again2, not_used_by_game).wem"
 mv 217686622.wem "Tribalstack Tropics - Corplet Camp (again3, not_used_by_game).wem"
 mv 225243248.wem "Tribalstack Tropics - Corplet Camp (again4, not_used_by_game).wem"
+mv 604947276.wem "Tribalstack Tropics - Corplet Camp (again5, not_used_by_game).wem"
+mv 697174408.wem "Tribalstack Tropics - Corplet Camp (again6, not_used_by_game).wem"
+mv 1004175105.wem "Tribalstack Tropics - Corplet Camp (again7, not_used_by_game).wem"
+
+
 
 mv 398883026.wem "Tribalstack Tropics - Monument Intro.wem"
 mv 162230282.wem "Tribalstack Tropics - Monument Intro.wem"
 mv 287648828.wem "Tribalstack Tropics - Monument.wem"
 mv 14799336.wem  "Tribalstack Tropics - Monument (again, not_used_by_game).wem"
 mv 480460865.wem "Tribalstack Tropics - Monument (again2, not_used_by_game).wem"
+mv 608537953.wem "Tribalstack Tropics - Monument (again3, not_used_by_game).wem"
+mv 922848822.wem "Tribalstack Tropics - Monument (again4, not_used_by_game).wem"
+mv 956730973.wem "Tribalstack Tropics - Monument (again5, not_used_by_game).wem"
+mv 1051348951.wem "Tribalstack Tropics - Monument (again6, not_used_by_game).wem"
 
-mv 298777168.wem "Tribalstack Tropics - Boss - The Great Rampo.wem"
-mv 386756905.wem "Tribalstack Tropics - Boss - The Great Rampo again.wem"
+
+
+
+mv 298777168.wem "Tribalstack Tropics - Boss (The Great Rampo).wem"
+mv 386756905.wem "Tribalstack Tropics - Boss (The Great Rampo) (again).wem"
+mv 728916668.wem "Tribalstack Tropics - Boss (The Great Rampo) (again2).wem"
 
 mv 395580153.wem "Tribalstack Tropics - Nimble Race 1.wem"
 mv 238147918.wem "Tribalstack Tropics - Nimble Race 1 (again, unlooped).wem"
 mv 13954808.wem  "Tribalstack Tropics - Nimble Race 1 (again2, unlooped). wem"
+mv 1022251749.wem  "Tribalstack Tropics - Nimble Race 1 (again3, unlooped). wem"
 
 
 
+
+
+mv 641067179.wem "Glitterglaze Glacier - Icymetric Palace Intro.wem"
 mv 148483046.wem "Glitterglaze Glacier - Icymetric Palace.wem"
 mv 31189334.wem  "Glitterglaze Glacier - Icymetric Palace (unlooped).wem"
 mv 688300463.wem "Glitterglaze Glacier - Normal.wem"
-mv 583504928.wem "Glitterglaze Glacier - Normal (unlooped).wem"
+mv 583504928.wem "Glitterglaze Glacier - ?? (unlooped).wem"
+mv 416075368.wem "Glitterglaze Glacier - Normal (unlooped).wem"
 mv 357001001.wem "Glitterglaze Glacier - Normal Intro.wem"
-mv 641067179.wem "Glitterglaze Glacier - Normal Intro2.wem"
-mv 405732086.wem "Glitterglaze Glacier - Boss - Brrreeze Blok.wem"
+mv 405732086.wem "Glitterglaze Glacier - Boss (Brrreeze Blok).wem"
 mv 484951464.wem "Glitterglaze Glacier - Aquatic.wem"
 mv 547612741.wem "Glitterglaze Glacier - Gloomy Gem Grotto.wem"
+
+
+mv 701458817.wem "Glitterglaze Glacier - Crystalline Cave Intro.wem"
 mv 676958470.wem "Glitterglaze Glacier - Crystalline Cave.wem"
+mv 693539848.wem "Glitterglaze Glacier - Crystalline Cave (?).wem"
 mv 297309298.wem "Glitterglaze Glacier - Crystalline Cave (unlooped).wem"
+mv 1006406196.wem "Glitterglaze Glacier - Crystalline Cave (Hurry).wem"
+
 
 
 
@@ -135,19 +180,42 @@ mv 84745841.wem "Moodymaze Marsh - Normal.wem"
 mv 251664514.wem "Moodymaze Marsh - Deep Swamp (unlooped).wem"
 mv 253636843.wem "Moodymaze Marsh - Swampy Station.wem"
 mv 740929881.wem "Moodymaze Marsh - Even Deeper Swap (expanded World).wem"
+mv 1004707711.wem "Moodymaze Marsh - Boss (Trev the Tenteyecle).wem"
 
 
+
+mv 134263814.wem "Capital Cashino - Aquatic.wem"
+mv 334721071.wem "Capital Cashino - Behind the scenes (Wager Laser, Power Grid).wem"
+mv 70864145.wem "Capital Cashino - Dance Floor.wem"
+mv 717876552.wem "Capital Cashino - Normal.wem"
+mv 791763668.wem "Capital Cashino - Safe Room.wem"
+mv 726122661.wem "Capital Cashino - Kartos Challenge.wem"
+mv 592774655.wem "Capital Cashino - Hurry.wem"
 
 mv 1015079427.wem "Dr. Quack's Quack-Fire Quiz.wem"
+mv 779320198.wem "Dr. Quack's Quack-Fire Quiz (again).wem"
 
 mv 581189516.wem "SFX - Jingle Bells.wem"
+mv 740153353.wem "SFX - Slurp Shot (Eating Berries).wem"
+mv 756341589.wem "SFX - Caught Ghost Writer.wem"
+mv 861876207.wem "SFX - Thunder.wem"
+mv 1025733604.wem "SFX - Collect Mollycool.wem"
 
 mv 647222234.wem "Rextro Arcade - ?.wem"
+mv 722727372.wem "Rextro Arcade - ?Klungo returns?.wem"
+mv 737521290.wem "Rextro Arcade - ??.wem"
+mv 745346874.wem "Rextro Arcade - ???.wem"
+mv 996207960.wem "Rextro Arcade - ?first game, that kart race?.wem"
+mv 782124416.wem "Rextro Arcade - Normal.wem"
+mv 523359880.wem "Rextro Arcade - Normal (again).wem"
 
-1004175105.wem
-1004707711.wem
+
+mv 594567569.wem "Galleon Galaxy - Normal.wem"
+mv 676601034.wem "Galleon Galaxy - Normal.wem"
+mv 606852958.wem "Galleon Galaxy - ?.wem"
+mv 1041573570.wem "Galleon Galaxy - ??.wem"
+
 1005096691.wem
-1006406196.wem
 1008935718.wem
 1010807917.wem
 1012144986.wem
@@ -155,14 +223,10 @@ mv 647222234.wem "Rextro Arcade - ?.wem"
 
 
 1018588088.wem
-1022251749.wem
-1025733604.wem
 1029255641.wem
 1035426249.wem
 1039566825.wem
-1041573570.wem
 1048738159.wem
-1051348951.wem
 
 105394285.wem
 1055128299.wem
@@ -211,14 +275,12 @@ mv 647222234.wem "Rextro Arcade - ?.wem"
 319733823.wem
 328632575.wem
 333680004.wem
-334721071.wem
 337472753.wem
 340906762.wem
 344059508.wem
 351730250.wem
 369183907.wem
 369864684.wem
-
 376828056.wem
 377232333.wem
 382241610.wem
@@ -229,16 +291,12 @@ mv 647222234.wem "Rextro Arcade - ?.wem"
 409663388.wem
 410801645.wem
 412038225.wem
-416075368.wem
 419992515.wem
 425157647.wem
 431439255.wem
-431661253.wem
 457968170.wem
-461690759.wem
 468160550.wem
 471112338.wem
-473513511.wem
 478008553.wem
 478721635.wem
 479353201.wem
@@ -253,7 +311,6 @@ mv 647222234.wem "Rextro Arcade - ?.wem"
 512189928.wem
 517798599.wem
 521037595.wem
-523359880.wem
 533380088.wem
 533849150.wem
 538090783.wem
@@ -265,15 +322,8 @@ mv 647222234.wem "Rextro Arcade - ?.wem"
 554160212.wem
 556002834.wem
 565897698.wem
-570519491.wem
-589136190.wem
-592774655.wem
-594567569.wem
 596833678.wem
-604947276.wem
 605193210.wem
-606852958.wem
-608537953.wem
 609116138.wem
 612946724.wem
 615517057.wem
@@ -284,7 +334,6 @@ mv 647222234.wem "Rextro Arcade - ?.wem"
 643101512.wem
 664424705.wem
 665137701.wem
-676601034.wem
 677278999.wem
 680176587.wem
 680649174.wem
@@ -293,32 +342,19 @@ mv 647222234.wem "Rextro Arcade - ?.wem"
 685969381.wem
 686836984.wem
 691969138.wem
-693539848.wem
 695192682.wem
-697174408.wem
-701458817.wem
 706107125.wem
-710135945.wem
 715352000.wem
 717672738.wem
-717876552.wem
 721125978.wem
-722727372.wem
 725652065.wem
-726122661.wem
-726295195.wem
-728916668.wem
 733128434.wem
 733296349.wem
 735617263.wem
 736186607.wem
 737268256.wem
-737521290.wem
 739556270.wem
-740153353.wem
-745346874.wem
 751992073.wem
-756341589.wem
 757807458.wem
 762275906.wem
 763586405.wem
@@ -329,13 +365,10 @@ mv 647222234.wem "Rextro Arcade - ?.wem"
 771533348.wem
 772239675.wem
 773695599.wem
-779320198.wem
-782124416.wem
 782948772.wem
 783681839.wem
 783948087.wem
 784927594.wem
-791763668.wem
 793653577.wem
 794176864.wem
 795208762.wem
@@ -346,7 +379,6 @@ mv 647222234.wem "Rextro Arcade - ?.wem"
 836398535.wem
 839239894.wem
 849647996.wem
-861876207.wem
 865430140.wem
 875728178.wem
 878104816.wem
@@ -357,14 +389,12 @@ mv 647222234.wem "Rextro Arcade - ?.wem"
 905180337.wem
 905636539.wem
 907736485.wem
-910563463.wem
 910619733.wem
 911034296.wem
 911768037.wem
 913469834.wem
 915098121.wem
 921848464.wem
-922848822.wem
 925526659.wem
 926548473.wem
 927464607.wem
@@ -372,23 +402,15 @@ mv 647222234.wem "Rextro Arcade - ?.wem"
 930992084.wem
 938733566.wem
 940311187.wem
-942605757.wem
 943527140.wem
-949309814.wem
 950477509.wem
 951630740.wem
 953233564.wem
-956730973.wem
 956995474.wem
 961257650.wem
-968037953.wem
 969611759.wem
 972374786.wem
-972616824.wem
 987976064.wem
 993276153.wem
-995363164.wem
-996207960.wem
-997014908.wem
 997760992.wem
 997850493.wem
